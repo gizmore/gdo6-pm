4377a7c0c8600ad79767a358f943d29fa463929f
gizmore <gizmore@wechall.net>
2019-08-31 16:28:51 +0200

    6.10-r42_german_lang

---
+++ lang/pm_de.php
@@ -0,0 +1,70 @@
+<?php
+return array(
+# Config Admin
+'cfg_pm_re' => 'Titelvoranstellung für Antworten',
+'cfg_pm_limit' => 'Maximal PN innerhalb des Timeouts',
+'cfg_pm_limit_timeout' => 'PM Limit Timeout',
+'cfg_pm_max_folders' => 'Maximale Anzahl eigener Ordner',
+'cfg_pm_for_guests' => 'Erlaube Gästen PN zu schreiben?',
+'cfg_pm_captcha' => 'Captcha für PN schreiben?',
+'cfg_pm_causes_mail' => 'Email bei PN senden?',
+'cfg_pm_bot_uid' => 'Bot UserId',
+'cfg_pm_own_bot' => 'Einen extra PN bot verwenden?',
+'cfg_pm_per_page' => 'PN pro Seite',
+'cfg_pm_welcome' => 'Willkommens-PN senden?',
+'cfg_pm_sig_len' => 'Maximale Signatur Länge',
+'cfg_pm_msg_len' => 'Maximale Nachrichten Länge',
+'cfg_pm_title_len' => 'Maximale Titel Länge',
+'cfg_pm_fname_len' => 'Maximale Ordnernamen Länge',
+'cfg_pm_delete' => 'PN wirklich löschen?',
+'cfg_pm_limit_per_level' => 'Nutzerlevel für eine PN mehr',
+# Config User
+'cfg_pm_level' => 'Benötigter Nutzerlevel um Ihnen eine PN zu senden',
+'cfg_pm_email' => 'Sie möchten per Email bei einer neuen PN benachrichtigt werden?',
+'cfg_pm_guests' => 'Gästen erlauben Ihnen eine PN zu schreiben?',
+'cfg_signature' => 'Ihre Signatur',
+# Navbar
+'link_overview' => 'Übersicht',
+'link_settings' => 'Einstellungen',
+'link_trashcan' => 'Mülleimer',
+'link_write_pm' => 'PN verfassen',
+# Trashcan
+'name_trashcan' => 'Mülleimer',
+'inbox_name' => 'Eingehend',
+'outbox_name' => 'Gesendet',
+'btn_restore' => 'Wiederherstellen',
+'btn_empty' => 'Leeren',
+'btn_delete' => 'Löschen',
+'btn_pm' => 'Private Nachrichten',
+'msg_pm_destroyed' => '%s PN wurde(n) gelöscht.',
+'msg_pm_restored' => '%s PN wurde(n) wiederhergestellt.',
+# Write
+'ft_pm_write' => 'Private Nachricht verfassen',
+'msg_pm_sent' => 'Ihre private Nachricht wurde gesendet.',
+# Settings
+'pm_email' => 'Email bei Pn?',
+'pm_level' => 'Benötigter Nutzerlevel um Ihnen eine PN zu schreiben',
+'signature' => 'Ihre Signatur',
+'pm_guests' => 'Gäste dürfen Ihnen PN schreiben?',
+# Read
+'pm_by' => 'Von: %s',
+'pm_to' => '  An: %s',
+'pm_sent' => 'gesendet am %s',
+# Overview
+'btn_move' => 'In Ordner verschieben',
+'msg_pm_deleted' => 'Es wurden %s PN geöschtet.',
+'err_pm_folder' => 'Der PN Ordner konnte nicht gefunden werden.',
+'pm_sent' => 'Gesendet, %s',
+'pm_received' => 'Empfangen, %s',
+################################################
+'pm_welcome_title' => 'Willkommen auf %s',
+'pm_welcome_message' => '
+Hallo %s,
+
+Wir heissen Dich herzlich Willkommen auf %s.
+Wir hoffen Du hast Spass mit dieser Webseite.
+
+Viele Grüße
+Das %2$s Team',
+################################################
+);
